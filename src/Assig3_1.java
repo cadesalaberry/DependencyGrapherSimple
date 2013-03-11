2ebfffa2c35d850c8cee292b9e132135dadb3d7c
C-A de Salaberry <cadesalaberry@yahoo.com>
2013-03-11 02:39:56 -0400

    Mod: Working Java.

--- src/Assig3_1.java
+++ src/Assig3_1.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 // Framework for assignment 3, question 1c
 public class Assig3_1 {
-    public Random r; // seeded or not 
+    public static Random r; // seeded or not 
 
     // Starting point. Accepts 1 or 2 command-line arguments,
     // the number of tasks (n) and an optional random seed may 
